adb9623f8329fcfba418fb655d4e4daeaac540ad
belaekaputri <belaekaputri28@gmail.com>
2021-10-30 17:34:05 +0700

    edit beberapa file

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@ const about=document.querySelector('#about');
 const contact=document.querySelector('#contact');
 const aboutContent=document.querySelector('#about-content');
 const isicontact=document.querySelector('#contact-content');
+const portfoliocontent=document.querySelector('#portfolio-content')
 about.addEventListener('click',()=>{
     //ketika li atau menu atau menu about diklik  jalankan winbox 
     const aboutBox=new WinBox({
@@ -30,5 +31,21 @@ contact.addEventListener('click',()=>{
         width: "25%",
         height: "50%",
         mount:isicontact
+})
+});
+
+
+portfolio.addEventListener('click',()=>{
+    const portfolioBox=new WinBox({
+        //objek dari library winbox
+        title:'Contact Me',
+        background:'rgb(243, 179, 179)',
+        width:'400px',
+        height:'400px',
+        x: "center",
+        y: "center",
+        width: "25%",
+        height: "50%",
+        mount:portfoliocontent
 })
 });
